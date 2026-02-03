f0d2656868a8033de9d19ce30e5c539931742971
Jayasrithiyagarajan <104126539+Jayasrithiyagarajan@users.noreply.github.com>
2026-02-03 12:31:35 +0530

    new changes in mail and auth controller

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,10 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
   service: 'gmail',
-  secure: false, // Port 587 uses STARTTLS
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD
