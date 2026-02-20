32b678ab50ad50f5d00a058270b5b5c8fb2cd3ef
Jayasrithiyagarajan <104126539+Jayasrithiyagarajan@users.noreply.github.com>
2026-02-20 12:05:05 +0530

    Changes in adminsubscribers,subsriber.controller.js and subscriber.routes.js

--- server/routes/subscriber.routes.js
+++ server/routes/subscriber.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { subscribe, getSubscribers } from '../controllers/subscriber.controller.js';
+import { subscribe, getSubscribers, sendMessageToAllSubscribers } from '../controllers/subscriber.controller.js';
 
 const router = express.Router();
 
 router.post('/subscribe', subscribe);
 router.get('/admin/subscribers', getSubscribers);
+router.post('/admin/send-message', sendMessageToAllSubscribers);
 
 export default router;
