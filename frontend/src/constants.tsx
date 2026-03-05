69cbd94cfbb2c1d472be65769b9a067e36851575
cloudandbeyond2 <145745372+cloudandbeyond2@users.noreply.github.com>
2026-03-05 18:05:16 +0530

    Update constants.tsx

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -43,9 +43,9 @@ export const paypalPlanIdTwo = "P-8T744865W27080359MWOCE5QRR";
 
 //RAZORPAY
 // export const razorpayKeyId = "rzp_test_SCLaCj6sIAD331";
-export const razorpayKeyId = "rzp_live_SMKNj0QLOVr3YK";
-export const razorpayPlanIdOne = "plan_SMJoon01vS1dhm";
-export const razorpayPlanIdTwo = "plan_SMJOKt0R4Ffxlb";
+export const razorpayKeyId = "rzp_test_SNXqWQ4BRcnHhA";
+export const razorpayPlanIdOne = "plan_SJqvapiTytNotQ";
+export const razorpayPlanIdTwo = "plan_SJsENuOG8v3573";
 
 
 // export const razorpayPlanIdOne = "plan_SDArK5ibfyHQlJ";
