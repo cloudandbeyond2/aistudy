8c94ea2b8e22cebff8e2aac22566d7da4a7b5ed7
C-Balasubramanian <balainfilabs@gmail.com>
2026-03-05 13:51:26 +0530

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => ({
         ]
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
